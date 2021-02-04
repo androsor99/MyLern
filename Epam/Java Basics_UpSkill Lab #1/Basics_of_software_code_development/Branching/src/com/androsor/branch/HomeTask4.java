1cffe0a134dd8fd5bdfab15dce2d7d49f193a959
androsor99 <androsor99@gmail.com>
2021-02-04 22:48:35 +0300

    Deleting the contents of folders in the mine branch

--- Epam/Java Basics_UpSkill Lab #1/Basics_of_software_code_development/Branching/src/com/androsor/branch/HomeTask4.java
+++ Epam/Java Basics_UpSkill Lab #1/Basics_of_software_code_development/Branching/src/com/androsor/branch/HomeTask4.java
@@ -12,40 +12,16 @@ public class HomeTask4 {
         double y = 0;
         double z = 0;
 
-<<<<<<< HEAD
-        if (checkNegative(A, B, x, y, z)) {
-        if ((x<=A && y<=B) || (y<=A && x<=B) || (x<=A && z<=B) || (z<=A && x<=B) || (z<=A && y<=B) || (y<=A && z<=B)) {
-=======
         if (A > 0 & B > 0 & x > 0 & y > 0 & z > 0) {
             if ((x<=A && y<=B) || (y<=A && x<=B) || (x<=A && z<=B) || (z<=A && x<=B) || (z<=A && y<=B) || (y<=A && z<=B)) {
->>>>>>> 20abfb964b0798d08157d9ff06408ca0759ec2e7
                 System.out.println(" Кирпич проскочит!))");
             }
             else {
                 System.out.println(" Пилите, Шура, гири!))");
             }
         }
-<<<<<<< HEAD
-
-
-
-    }
-
-
-    private static boolean checkNegative(double ... param) {
-
-        for (int i =0; i >= param.length-1; i++) {
-            if (param[i] <= 0) {
-                System.out.println(" Размеры отверстия и кирпича должны быть оличными от нуля в положительную сторону");
-                return false;
-
-            }
-        }
-        return true;
-=======
         else {
             System.out.println(" Размеры отверстия и кирпича должны быть оличными от нуля в положительную сторону");
         }
->>>>>>> 20abfb964b0798d08157d9ff06408ca0759ec2e7
     }
 }
--- Epam/Java Basics_UpSkill Lab #1/Basics_of_software_code_development/Linear_Programs/src/com/androsor/liner/HomeTask6.java
+++ Epam/Java Basics_UpSkill Lab #1/Basics_of_software_code_development/Linear_Programs/src/com/androsor/liner/HomeTask6.java
@@ -1,11 +1,5 @@
 package com.androsor.liner;
 
-<<<<<<< HEAD
-import java.util.Comparator;
-import java.util.Random;
-
-=======
->>>>>>> 20abfb964b0798d08157d9ff06408ca0759ec2e7
 /**
  * For this area, compose a linear program that prints true if the point with coordinates (x, y) belongs to the filled area, and false otherwise.
  */
@@ -15,27 +9,7 @@ public class HomeTask6 {
         double x = 10;
         double y = -15;
 
-<<<<<<< HEAD
-        System.out.println(Math.abs(x) <= 4 && y <= 0 && y >= 3 || Math.abs(x) <= 2 && y >= 0 && y <= 4);
-
-    }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-=======
         System.out.println(Math.abs(x) <= 4 && y <= 0 && y >=3 || Math.abs(x) <= 2 && y >= 0 && y <= 4);
     }
 }
 
->>>>>>> 20abfb964b0798d08157d9ff06408ca0759ec2e7
