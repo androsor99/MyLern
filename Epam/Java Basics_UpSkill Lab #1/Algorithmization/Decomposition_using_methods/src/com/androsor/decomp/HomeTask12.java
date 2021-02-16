a9cc234db043a32369f4fb77b9a6e6a18d466c94
androsor99 <androsor99@gmail.com>
2021-02-16 23:27:14 +0300

    code fix ver.1.3

--- Epam/Java Basics_UpSkill Lab #1/Algorithmization/Decomposition_using_methods/src/com/androsor/decomp/HomeTask12.java
+++ Epam/Java Basics_UpSkill Lab #1/Algorithmization/Decomposition_using_methods/src/com/androsor/decomp/HomeTask12.java
@@ -1,8 +1,10 @@
 package com.androsor.decomp;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.androsor.decomp.Data.*;
+import static com.androsor.decomp.Data.inputDataInt;
+import static java.lang.Math.*;
 
 /**
  * Given natural numbers K and N. Write a method (methods) for forming an array A, whose elements are numbers whose sum
@@ -12,75 +14,41 @@ public class HomeTask12 {
 
     public static void main (String[]args) {
 
-        int numberK = inputDataInt(" Введите число numberK = ");
+        int numberK = abs(inputDataInt(" Введите число numberK = "));
 
-        int numberN = inputDataInt(" Введите число numberN = ");
-        while (numberN <= numberK) {
-            System.out.println(" Число должно быть больше нуля и больше числа numberK");
-            numberN = inputDataInt(" Введите число numberK = ");
-        }
-        int b = inputDataInt(" Введите длину желаемого массива b = ");
-        while (b < 0) {
-            System.out.println(" Число должно быть больше нуля.");
-            b = inputDataInt(" Введите длину желаемого массива b = ");
-        }
+        int numberN = abs(inputDataInt(" Введите число numberN = "));
 
-        System.out.println(" Массив из элементов согласно условию задачи:");
-
-        arrayRandom(numberK,numberN,b);
-    }
-    // Метод создания и заполнения массива из цифр числа.
-    public static int [] myArray(int number) {
-        int temp = number;
-        int count = 0;
-
-        while (temp > 0) {
-            count++;
-            temp = temp / 10;
-        }
-
-        int[] myArray = new int[count];
-
-        for (int j = count - 1 ; number > 0; j--) {
-            myArray[j] = number % 10;
-            number = number / 10;
-        }
-        return myArray;
+        List<Integer> myArray = fillArray(numberK, numberN);
+        printArray(myArray);
     }
 
-    // Метод нахождения суммы элементов массива.
-    public  static int sumMyArray (int [] myArray){
+    private static List<Integer> fillArray(int numberK, int numberN) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= numberN; i++) {
+            if (getSumDigitsOfNumber(i) == numberK) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
 
+    private static int getSumDigitsOfNumber(int number) {
         int sum = 0;
-        for (int i = 0; i < myArray.length; i++) {
-            sum = sum + myArray[i];
+        while (number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
         }
         return sum;
     }
-    // Метод заполнения массива элементами которого являются числа, сумма цифр которых равна К и которые не большее N.
-    public static void arrayRandom (int k, int n, int b) {
 
-        int [] arrayRandom = new int[b];
-        int count = 0;
-
-        Random random = new Random();
-
-        while (count < arrayRandom.length){
-
-            int number = random.nextInt(100000);
-            int number1 = sumMyArray(myArray(number));
-
-            if(number1 == k & number < n){
-
-                arrayRandom[count] = number;
-                count++;
-            }
-        }
-
-        for (int i = 0; i < arrayRandom.length; i++){
-
-            System.out.print(arrayRandom[i] + " ");
+    private static void printArray(List<Integer> list) {
+        if (list.isEmpty()) {
+            System.out.println(" Создать массив невозможно. На заданном диапазоне не существует чисел удовлетворяющих вводимым данным!");
+        } else {
+            System.out.println(" Массив из элементов согласно условию задачи:");
+            System.out.println("----------------------------------------");
+            System.out.println(" " + list.toString());
+            System.out.println("----------------------------------------");
         }
     }
 }
-
--- Epam/Java Basics_UpSkill Lab #1/Algorithmization/Decomposition_using_methods/src/com/androsor/decomp/HomeTask13.java
+++ Epam/Java Basics_UpSkill Lab #1/Algorithmization/Decomposition_using_methods/src/com/androsor/decomp/HomeTask13.java
@@ -1,46 +1,46 @@
 package com.androsor.decomp;
 
-import java.util.Scanner;
+import static com.androsor.decomp.Data.inputDataInt;
+import static java.lang.Math.abs;
 
 /**
  * Two primes are called "twins" if they differ from each other by 2 (for example, 41 and 43). Find and print all pairs
  * of "twins" from the segment [n, 2n], where n is a given natural number greater than 2.
  * Use decomposition to solve the problem.
  */
-
 public class HomeTask13 {
 
     public static void main(String[] args) {
 
-        int n;
 
-        n = dataInput(" Введите число N = ");
-        System.out.printf(" На отрезке от %d до %d существуют следующие пары: \n", n, 2 * n);
-        for (int i = n; i < (n * 2) - 2; i++) {
-            if (primeNumber(i)) {
-                System.out.printf(" Пара \"чисел-близнецов\": %d %d  \n", i, (i + 2));
-            }
-        }
+        int number = getNumber(abs(inputDataInt(" Введите число N = ")));
+
+        printTwins(number);
     }
 
-    // Метод ввода данных.
-    public static int dataInput(String massage) {
-        System.out.print(massage);
-        Scanner sc = new Scanner(System.in);
-        while (!sc.hasNextInt()) {
-            sc.next();
-            System.out.println(" Введенные данные не являются натуральными числами. Повторите ввод.");
+    private static int getNumber(int number) {
+        while (number <= 2) {
+            System.out.println(" Начало диапазона вычислений должно быть больше 2 по условию задачи");
+            number = abs(inputDataInt(" Введите число N = "));
         }
-        return sc.nextInt();
+        return number;
     }
 
-    // Метод проверки на простоту.
-    public static boolean primeNumber(int k) {
-        for (int j = 2; j < Math.sqrt(k)+1; j++) {
-            if (k % j == 0) {
+    private static boolean isPrime(int value) { // метод для проверки на простоту.
+        for (int j = 2; j < Math.sqrt(value) + 1; j++) {
+            if (value % j == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static void printTwins(int number) {
+        System.out.printf(" На отрезке от %d до %d существуют следующие пары: \n", number, 2 * number);
+        for (int i = number; i < (number * 2) - 2; i++) {
+            if (isPrime(i) && isPrime(i + 2)) {
+                System.out.printf(" Пара \"чисел-близнецов\": %d %d  \n", i, (i + 2));
+            }
+        }
     }
 }
