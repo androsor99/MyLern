f72bb7e57acb7c357e3251babba86838f23dde5f
androsor99 <androsor99@gmail.com>
2021-02-18 19:21:41 +0300

    code fix ver 4.3

--- Epam/Java Basics_UpSkill Lab #1/Algorithmization/Arrays_of_arrays/src/com/androsor/multiarray/ArrayPrinter.java
+++ Epam/Java Basics_UpSkill Lab #1/Algorithmization/Arrays_of_arrays/src/com/androsor/multiarray/ArrayPrinter.java
@@ -7,9 +7,9 @@ public class ArrayPrinter {
 
     public  static void printArrayInt (int [][] numbers) {
         System.out.println("-----------------------------------------------");
-        for (int[] items : numbers) {
+        for (int[] number : numbers) {
             for (int j = 0; j < numbers.length; j++) {
-                System.out.printf("%7d", items[j]);
+                System.out.printf("%7d", number[j]);
             }
             System.out.println();
         }
@@ -18,9 +18,9 @@ public class ArrayPrinter {
 
     public  static void printArrayDouble (double [][] numbers) {
         System.out.println("----------------------------------------------");
-        for (double[] items : numbers) {
+        for (double[] number : numbers) {
             for (int j = 0; j < numbers.length; j++) {
-                System.out.printf("%9.3f", items[j]);
+                System.out.printf("%9.3f", number[j]);
             }
             System.out.println();
         }
