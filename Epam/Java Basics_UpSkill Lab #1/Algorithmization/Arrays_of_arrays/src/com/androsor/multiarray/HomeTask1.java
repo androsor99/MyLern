da51aa8d93f275aa2713d6c79fc7ee083a79123c
androsor99 <androsor99@gmail.com>
2021-02-21 19:31:41 +0300

    code fix ver 4.6

--- Epam/Java Basics_UpSkill Lab #1/Algorithmization/Arrays_of_arrays/src/com/androsor/multiarray/HomeTask1.java
+++ Epam/Java Basics_UpSkill Lab #1/Algorithmization/Arrays_of_arrays/src/com/androsor/multiarray/HomeTask1.java
@@ -24,11 +24,11 @@ public class HomeTask1{
 
     private static void printOddColumns(int[][] numbers) {
         int count = 0; // счетчик для количества столбцов
-        for (int j = 0; j < numbers.length; j += 2) {
+        for (int j = 0; j < numbers[0].length; j += 2) {
             if (numbers[0][j] > numbers[numbers.length - 1][j]) {
                 count ++;
                 System.out.println(j + 1 +"-ый нечетный столбец у которого первый элемент больше последнего");
-                for (int i = 0; i < numbers[0].length; i++) {
+                for (int i = 0; i < numbers.length; i++) {
                     System.out.println(numbers[i][j]);
                 }
             }
