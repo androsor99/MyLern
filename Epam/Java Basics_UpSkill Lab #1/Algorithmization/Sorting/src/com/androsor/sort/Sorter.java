4fe5c30515da209c3a72e84de0c3ad79142eb5b5
androsor99 <androsor99@gmail.com>
2021-03-04 09:30:15 +0300

    code fix ver.2.9

--- Epam/Java Basics_UpSkill Lab #1/Algorithmization/Sorting/src/com/androsor/sort/Sorter.java
+++ Epam/Java Basics_UpSkill Lab #1/Algorithmization/Sorting/src/com/androsor/sort/Sorter.java
@@ -2,5 +2,11 @@ package com.androsor.sort;
 
 public interface Sorter {
 
+     /**
+      * Returns an array in ascending or descending order.
+      * @param numbers sortable array.
+      * @param sortSelection selection of sorting direction.
+      * @return array in ascending or descending order.
+      */
      int[] sort(int[] numbers, boolean sortSelection);
 }
