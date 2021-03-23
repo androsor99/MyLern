f11904f59f94b61321e0c86ef5cc5462daf10559
androsor99 <androsor99@gmail.com>
2021-03-23 23:14:09 +0300

    code fix ver.1.5

--- Epam/Java Basics_UpSkill Lab #1/Strings_and_basics_of_text_processing/Working_with_a_string_as_an_array_of_characters/src/com/androsor/string_char/IOUtils.java
+++ Epam/Java Basics_UpSkill Lab #1/Strings_and_basics_of_text_processing/Working_with_a_string_as_an_array_of_characters/src/com/androsor/string_char/IOUtils.java
@@ -2,17 +2,11 @@ package com.androsor.string_char;
 
 import java.util.Scanner;
 
-public class IOUtils implements AutoCloseable {
+public class IOUtils {
 
     public static String enterFromConsole(String message) {
-        try (Scanner scanner = new Scanner(System.in)) {
+       Scanner scanner = new Scanner(System.in);
             System.out.println(message);
             return scanner.nextLine();
-        }
-    }
-
-    @Override
-    public void close() throws Exception {
-
     }
 }
